445e8a2da73e401fbe74fd8b0f5fd468d33fbc9d
r1m2@GITHUB <rafaelmorenods@gmail.com>
2025-05-02 20:31:58 -0300

    ATUALIZACAO 1

--- app.py
+++ app.py
@@ -64,9 +64,14 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 @app.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == form_data.username).first()
+    
     if not user or not verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail="Credenciais inválidas")
-    token = create_access_token({"sub": user.username})
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciais inválidas"
+        )
+    
+    token = create_access_token(data={"sub": user.username})
     return {"access_token": token, "token_type": "bearer"}
 
 @app.get("/protegido")
