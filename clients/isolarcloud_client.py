a455ad89d1b7505c94e0113cdf149e0967a1e1db
r1m2@GITHUB <rafaelmorenods@gmail.com>
2025-05-03 14:12:34 -0300

    TESTE CLAREON

--- clients/isolarcloud_client.py
+++ clients/isolarcloud_client.py
@@ -5,12 +5,17 @@ from utils import parse_float
 from datetime import datetime, timedelta
 from pytz import timezone
 
+
+
 class ApiSolarCloud:
+    # accesskey = "4ey7zficrz2np518aqku2a8997ha1ebz" RMS
+    accesskey =  "hd3cgs3yjgx8cv7z47t8hpsxkig14qxa"
     base_url = "https://gateway.isolarcloud.com.hk/openapi/"
-    appkey = "03A0E186C87230B4DE9E028F90491A58"
+    # appkey = "03A0E186C87230B4DE9E028F90491A58" RMS
+    appkey = "C79920B2B9D982D2603539BFE7066052"
     headers = {
         "Content-Type": "application/json",
-        "x-access-key": "4ey7zficrz2np518aqku2a8997ha1ebz",
+        "x-access-key": accesskey,
         "sys_code": "901"
     }
 
