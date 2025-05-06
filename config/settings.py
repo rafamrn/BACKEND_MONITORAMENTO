65af27a94f965ca22b447d867af55c5eeb7163de
r1m2@GITHUB <rafaelmorenods@gmail.com>
2025-05-05 23:07:50 -0300

    RMS 01

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ class Settings:
     HUAWEI_USER = "rms7energia"
     HUAWEI_PASS = "rms7@API"
     ISOLAR_USER = "contato@rms7energia.com"
-    ISOLAR_PASS = "rms7@SGUNROW"
+    ISOLAR_PASS = "rms7@SUNGROW"
     DEYE_USER = "contato@rms7energia.com"
     DEYE_PASS = "c964ff72b39a005ad966bd2333e865f91c471085fb9b82d76c4ec75c2638321f"
     DEYE_APPID = "202504302900004"
