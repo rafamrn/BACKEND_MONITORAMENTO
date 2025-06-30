ad4573f77539ded9ffa7c528e412f41a3baa931c
r1m2@GITHUB <rafaelmorenods@gmail.com>
2025-06-30 15:55:21 -0300

    teste

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def get_db():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:8080/",
         "https://frontendmonitoramento-production.up.railway.app"
     ],
     allow_credentials=True,
